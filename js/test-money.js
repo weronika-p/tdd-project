02d2af31c3b2edea249801941bcd2e6be3a3b2e1
weronika-pawlak <w.pawlak@intive.com>
2023-12-25 17:52:00 +0100

    test: finish times and divide methods

--- js/test-money.js
+++ js/test-money.js
@@ -1,12 +1,24 @@
 const assert = require('assert');
-class Dollar {
-    constructor(amount) {
-        this.amount = amount;
+class Money {
+    constructor(amount, currency) {
+        this.amount = amount,
+        this.currency = currency
     }
     times(multiplier) {
-        return new Dollar(this.amount * multiplier);
+        return new Money(this.amount * multiplier, this.currency);
     }
-};
-let fiver = new Dollar(5);
-let tenner = fiver.times(2);
-assert.strictEqual(tenner.amount, 10); // comparing actual value with expected value
+    divide(divider) {
+        return new Money(this.amount / divider, this.currency);
+    }
+}
+let fiveDollars = new Money(5, 'USD');
+let tenDollars = new Money(10, 'USD');
+assert.deepStrictEqual(fiveDollars.times(2), tenDollars); // comparing actual value with expected value
+let tenEuros = new Money(10, 'EUR');
+let twentyEuros = new Money(20, 'EUR');
+assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
+assert.strictEqual(twentyEuros.currency, 'EUR');
+let originalMoney = new Money(4002, 'KRW');
+let actualMoneyAfterDivision = originalMoney.divide(4);
+let expectedMoneyAfterDivision = new Money(1000.5, 'KRW');
+assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
